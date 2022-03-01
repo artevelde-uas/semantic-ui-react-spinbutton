e384cd73dcbb5d99031b81dbcaa4663fdb5ee735
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-03-01 13:15:45 +0100

    Use 4 space indentation

--- packages/demo/src/App.js
+++ packages/demo/src/App.js
@@ -3,22 +3,22 @@ import { Spinbutton } from 'semantic-ui-react-spinbutton';
 
 
 function App() {
-  return (
-    <Container text>
-      <Header as='h2'>Header</Header>
-      <Item.Group>
-        <Item>
-          <Item.Content>
-            <Item.Header as='a'>Default</Item.Header>
-            <Item.Meta>Spinbutton with default settings</Item.Meta>
-            <Item.Description>
-              <Spinbutton placeholder='Placeholder' />
-            </Item.Description>
-          </Item.Content>
-        </Item>
-      </Item.Group>
-    </Container>
-  );
+    return (
+        <Container text>
+            <Header as='h2'>Header</Header>
+            <Item.Group>
+                <Item>
+                    <Item.Content>
+                        <Item.Header as='a'>Default</Item.Header>
+                        <Item.Meta>Spinbutton with default settings</Item.Meta>
+                        <Item.Description>
+                            <Spinbutton placeholder='Placeholder' />
+                        </Item.Description>
+                    </Item.Content>
+                </Item>
+            </Item.Group>
+        </Container>
+    );
 }
 
 export default App;
