b2e9cfaaa0c6c467891848f7926fdcb7520588e2
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-03-01 11:51:54 +0100

    Add `Spinbutton` to app

--- packages/demo/src/App.js
+++ packages/demo/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Spinbutton } from 'semantic-ui-react-spinbutton';
 
 
 
@@ -11,14 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Spinbutton placeholder='Placeholder' />
       </header>
     </div>
   );
