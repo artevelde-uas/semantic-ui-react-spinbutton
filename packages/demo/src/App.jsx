1d334bf2a3ff26aa3f09263ccd77877533eef65c
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-03-02 01:21:20 +0100

    Use arrow function expression

--- packages/demo/src/App.jsx
+++ packages/demo/src/App.jsx
@@ -2,23 +2,19 @@ import { Container, Header, Item } from 'semantic-ui-react'
 import { Spinbutton } from 'semantic-ui-react-spinbutton';
 
 
-function App() {
-    return (
-        <Container text>
-            <Header as='h2'>Semantic UI React Spinbutton</Header>
-            <Item.Group>
-                <Item>
-                    <Item.Content>
-                        <Item.Header>Default Spinbutton</Item.Header>
-                        <Item.Meta>Spinbutton with default settings</Item.Meta>
-                        <Item.Description>
-                            <Spinbutton placeholder='Placeholder' />
-                        </Item.Description>
-                    </Item.Content>
-                </Item>
-            </Item.Group>
-        </Container>
-    );
-}
-
-export default App;
+export default () => (
+    <Container text>
+        <Header as='h2'>Semantic UI React Spinbutton</Header>
+        <Item.Group>
+            <Item>
+                <Item.Content>
+                    <Item.Header>Default Spinbutton</Item.Header>
+                    <Item.Meta>Spinbutton with default settings</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton placeholder='Placeholder' />
+                    </Item.Description>
+                </Item.Content>
+            </Item>
+        </Item.Group>
+    </Container>
+);
