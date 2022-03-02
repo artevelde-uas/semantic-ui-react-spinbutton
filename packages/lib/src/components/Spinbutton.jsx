54424ba654106ef5639979a2d7af33f7340464c7
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-03-02 01:09:15 +0100

    Add increase/decrease buttons

--- packages/lib/src/components/Spinbutton.jsx
+++ packages/lib/src/components/Spinbutton.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Input } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 
 
 export default (props) => (
-    <Input {...props} />
+    <Input {...props}
+        label={
+            <div>
+                <Button icon='angle up' />
+                <Button icon='angle down' />
+            </div>
+        }
+        labelPosition='right'
+    />
 );
