25a0b6a585dfcaec6fae34468c62170221198dc4
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-03-08 14:32:12 +0100

    Add more examples

--- packages/demo/src/App.jsx
+++ packages/demo/src/App.jsx
@@ -1,11 +1,109 @@
-import { Container, Header, Item } from 'semantic-ui-react'
-import { Spinbutton } from 'semantic-ui-react-spinbutton';
+import { Container, Header, Item, Input } from 'semantic-ui-react';
+import { Spinner, Spinbutton } from 'semantic-ui-react-spinbutton';
 
 
 export default () => (
     <Container text>
         <Header as='h1'>Semantic UI React Spinbutton</Header>
         <Item.Group>
+            <Item>
+                <Item.Content>
+                    <Item.Header>Spinner only</Item.Header>
+                    <Item.Meta>Vertical spinner</Item.Meta>
+                    <Item.Description>
+                        <Spinner orientation='vertical' />
+                    </Item.Description>
+                    <Item.Meta>Horizontal spinner</Item.Meta>
+                    <Item.Description>
+                        <Spinner orientation='horizontal' />
+                    </Item.Description>
+                </Item.Content>
+            </Item>
+            <Item>
+                <Item.Content>
+                    <Item.Header>Horizontal spinbuttons</Item.Header>
+                    <Item.Meta>Horizontal spinbutton with horizontal spinner on the left</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='left'
+                            buttonOrientation='horizontal'
+                        />
+                    </Item.Description>
+                    <Item.Meta>Horizontal spinbutton with vertical spinner on the left</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='right'
+                            buttonOrientation='vertical'
+                        />
+                    </Item.Description>
+                    <Item.Meta>Horizontal spinbutton with horizontal spinner on the right</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='right'
+                            buttonOrientation='horizontal'
+                        />
+                    </Item.Description>
+                    <Item.Meta>Horizontal spinbutton with vertical spinner on the right</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='left'
+                            buttonOrientation='vertical'
+                        />
+                    </Item.Description>
+                    <Item.Meta>Horizontal spinbutton with wrapped spinner</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='wrapped'
+                            buttonOrientation='horizontal'
+                        />
+                    </Item.Description>
+                </Item.Content>
+            </Item>
+            <Item>
+                <Item.Content>
+                    <Item.Header>Vertical spinbuttons</Item.Header>
+                    <Item.Meta>Vertical spinbutton with horizontal spinner at the top</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='top'
+                            buttonOrientation='horizontal'
+                            style={{ width: '100px' }}
+                        />
+                    </Item.Description>
+                    <Item.Meta>Vertical spinbutton with vertical spinner at the top</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='top'
+                            buttonOrientation='vertical'
+                            style={{ width: '100px' }}
+                        />
+                    </Item.Description>
+                    <Item.Meta>Vertical spinbutton with horizontal spinner at the bottom</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='bottom'
+                            buttonOrientation='horizontal'
+                            style={{ width: '100px' }}
+                        />
+                    </Item.Description>
+                    <Item.Meta>Vertical spinbutton with vertical spinner at the bottom</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='bottom'
+                            buttonOrientation='vertical'
+                            style={{ width: '100px' }}
+                        />
+                    </Item.Description>
+                    <Item.Meta>Vertical spinbutton with wrapped spinner</Item.Meta>
+                    <Item.Description>
+                        <Spinbutton
+                            buttonPosition='wrapped'
+                            buttonOrientation='vertical'
+                            style={{ width: '100px' }}
+                        />
+                    </Item.Description>
+                </Item.Content>
+            </Item>
             <Item>
                 <Item.Content>
                     <Item.Header>Default Spinbutton</Item.Header>
@@ -24,9 +122,9 @@ export default () => (
                     <Item.Description>
                         <Spinbutton
                             value='10'
-                            handleUp={value => (Number(value) + 1) || 0}
-                            handleDown={value => (Number(value) - 1) || 0}
-                            handleChange={value => Math.trunc(value) || 0}
+                            onUp={value => (Number(value) + 1) || 0}
+                            onDown={value => (Number(value) - 1) || 0}
+                            onChange={value => Math.trunc(value) || 0}
                         />
                     </Item.Description>
                     <Item.Meta>Spinbutton with numbers (not editable)</Item.Meta>
@@ -34,8 +132,8 @@ export default () => (
                         <Spinbutton
                             placeholder='Placeholder'
                             value='10'
-                            handleUp={value => (Number(value) + 1) || 0}
-                            handleDown={value => (Number(value) - 1) || 0}
+                            onUp={value => (Number(value) + 1) || 0}
+                            onDown={value => (Number(value) - 1) || 0}
                             readOnly
                         />
                     </Item.Description>
